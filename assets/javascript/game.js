03384337cdcbf6d3a6a737ef7aac0b0a613be802
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-02-10 16:39:51 -0600

    Created start game (reset game) function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,17 @@ var started = false;
 var ended = false;
 
 var wordBank = ["popsicle", "cheesecake", "brownie", "danish", "macaroon", "cupcake", "bonbon", "gingerbread", "pudding", "beignet", "churro", "sorbet", "gelato", "crepe", "mochi", "milkshake", "tiramisu", "sundae", "parfait", "brittle", "turnover", "cobbler"];
+
+// How to get the game to start or reset
+var reset = function() {
+    remGuesses = 10;
+    started = false;
+    // Generate a random word from the word bank
+    randWord = Math.floor(Math.random() * (wordBank.length));
+    guessedLet = [];
+    guessingword = [];
+    // Create underscores for blanks at each index of the random word
+    for (var i = 0; i< wordBank[randWord].length; i++) {
+        guessingWord.push("_");
+    }
+}
